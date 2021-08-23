07e84d662a34002d13c8e9d83fa01fa7ca479492
sukaidev <haihong.su97@gmail.com>
2021-08-23 14:01:48 +0800

    jetpack samples

---
+++ JetpackSamples/app/src/main/java/com/sukaidev/jetpacksamples/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sukaidev.jetpacksamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
